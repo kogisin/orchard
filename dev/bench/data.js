cc671359d6721ac1e1af16eb0f2a161a7d7aae09
github-action-benchmark <github@users.noreply.github.com>
2025-03-25 12:50:47 +0000

    add Orchard Benchmarks (cargo) benchmark result for 86705fbb70388d9b8561a698305ec5ab3d9d85b3

---
+++ dev/bench/data.js
@@ -0,0 +1,188 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1742907047144,
+  "repoUrl": "https://github.com/kogisin/orchard",
+  "entries": {
+    "Orchard Benchmarks": [
+      {
+        "commit": {
+          "author": {
+            "email": "defigengen@gmail.com",
+            "name": "Gena",
+            "username": "kogisin"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "86705fbb70388d9b8561a698305ec5ab3d9d85b3",
+          "message": "Create test.yml",
+          "timestamp": "2025-03-25T13:37:50+01:00",
+          "tree_id": "59efdff0db248c6219855345d7513253c7fe197d",
+          "url": "https://github.com/kogisin/orchard/commit/86705fbb70388d9b8561a698305ec5ab3d9d85b3"
+        },
+        "date": 1742907046078,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "proving/bundle/1",
+            "value": 2871899180,
+            "range": "± 225681083",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "proving/bundle/2",
+            "value": 2848552352,
+            "range": "± 19970661",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "proving/bundle/3",
+            "value": 4104226488,
+            "range": "± 29243886",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "proving/bundle/4",
+            "value": 5342050703,
+            "range": "± 14896136",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "verifying/bundle/1",
+            "value": 24506250,
+            "range": "± 1119038",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "verifying/bundle/2",
+            "value": 24644337,
+            "range": "± 290066",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "verifying/bundle/3",
+            "value": 27836831,
+            "range": "± 482166",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "verifying/bundle/4",
+            "value": 31504826,
+            "range": "± 329834",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "note-decryption/valid",
+            "value": 1480953,
+            "range": "± 9733",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "note-decryption/invalid",
+            "value": 125545,
+            "range": "± 287",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "note-decryption/compact-valid",
+            "value": 1476727,
+            "range": "± 10376",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "compact-note-decryption/invalid",
+            "value": 1327896913,
+            "range": "± 2595998",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "batch-note-decryption/valid/10",
+            "value": 15624848,
+            "range": "± 280674",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "batch-note-decryption/invalid/10",
+            "value": 2131549,
+            "range": "± 7914",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "batch-note-decryption/compact-valid/10",
+            "value": 15606948,
+            "range": "± 63682",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "batch-note-decryption/compact-invalid/10",
+            "value": 2096581,
+            "range": "± 20659",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "batch-note-decryption/valid/50",
+            "value": 78073944,
+            "range": "± 131856",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "batch-note-decryption/invalid/50",
+            "value": 10599110,
+            "range": "± 107505",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "batch-note-decryption/compact-valid/50",
+            "value": 77965656,
+            "range": "± 217421",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "batch-note-decryption/compact-invalid/50",
+            "value": 10421588,
+            "range": "± 26415",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "batch-note-decryption/valid/100",
+            "value": 156158542,
+            "range": "± 310443",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "batch-note-decryption/invalid/100",
+            "value": 21204346,
+            "range": "± 73533",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "batch-note-decryption/compact-valid/100",
+            "value": 155994155,
+            "range": "± 1497722",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "batch-note-decryption/compact-invalid/100",
+            "value": 20845153,
+            "range": "± 30044",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "derive_fvk",
+            "value": 463351,
+            "range": "± 11996",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "default_address",
+            "value": 487258,
+            "range": "± 3516",
+            "unit": "ns/iter"
+          }
+        ]
+      }
+    ]
+  }
+}
